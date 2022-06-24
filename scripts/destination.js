1bbbd26d548bfad8cd21c3eb740c2d6e58994db1
jlopcun <62543437+jlopcun@users.noreply.github.com>
2022-06-24 16:20:26 +0200

    destination.js "refactor" fetchData"refactor"

--- scripts/destination.js
+++ scripts/destination.js
@@ -1,4 +1,4 @@
-import { fetchData } from "../scripts/fetchData.js"
+import {updatePage } from "../scripts/fetchData.js"
 
 const $planetMenu = document.getElementById('planetMenu'),
 $planetContent = document.getElementById('planetContent')
@@ -7,27 +7,25 @@ $planetContent = document.getElementById('planetContent')
 
 window.addEventListener('DOMContentLoaded',()=>{
     $planetMenu.addEventListener('click',async (e)=>{
-        if(e.currentTarget===e.target) return 
-        const allElements = Array.from(e.currentTarget.children)
-        allElements.forEach(el=>{
-            el.classList.remove('actual')
-        })
-        e.target.classList.add('actual')
+        updatePage(e.currentTarget,e.target,$planetContent)
+        // if(e.currentTarget===e.target) return
+        
+        // const dataobj = e.currentTarget.dataset.dataobj
+        
+        // const allElements = Array.from(e.currentTarget.children)
+        // allElements.forEach(el=>{
+        //     el.classList.remove('actual')
+        // })
+        // e.target.classList.add('actual')
 
         
-        const data = await  fetchData('destinations',e.target.dataset.planet)
-        
-        const elementsToReplaceData = {}
 
-        $planetContent.querySelectorAll('[data-planetInf]').forEach(el=>{
-            elementsToReplaceData[el.dataset.planetinf] = el
-        })
-         
-        elementsToReplaceData.image.src = data.images.webp
-        elementsToReplaceData.name.textContent = data.name
-        elementsToReplaceData.desc.textContent = data.description
-        elementsToReplaceData.dist.textContent = data.distance
-        elementsToReplaceData.time.textContent = data.travel
-        
+        // const data = await  fetchData('destinations',e.target.dataset.planet)
+
+        // Object.entries(data).forEach(part=>{
+        //     const relativeElement =  $planetContent.querySelector(`[data-${dataobj}=${part[0]}]`)
+        //     if(relativeElement.tagName==='IMG') relativeElement.src = part[1].webp
+        //     else relativeElement.textContent = part[1]
+        // })
     })
 })
--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -1,12 +1,35 @@
-export {fetchData}
+export {updatePage}
 
-async function fetchData(final,planet){
+async function fetchData(final,query){
     try{
         const fetching = await fetch(`../starter-code/data.json`),
         json = await fetching.json()
-        const data = json[final].find(x=>x.name===planet)
+        const data = json[final].find(x=>x.name===query)
         return data
     }catch(err){
         console.error(`An error has ocurred:${err}`)
     }
+}
+
+
+async function updatePage(menuContainer,menuElement,parentElement){
+    if(menuContainer===menuElement) return
+        
+        const dataobj = menuContainer.dataset.dataobj
+        
+        const allElements = Array.from(menuContainer.children)
+        allElements.forEach(el=>{
+            el.classList.remove('actual')
+        })
+        menuElement.classList.add('actual')
+
+        
+
+        const data = await  fetchData('destinations',menuElement.dataset.planet)
+
+        Object.entries(data).forEach(part=>{
+            const relativeElement =  parentElement.querySelector(`[data-${dataobj}=${part[0]}]`)
+            if(relativeElement.tagName==='IMG') relativeElement.src = part[1].webp
+            else relativeElement.textContent = part[1]
+        })
 }
